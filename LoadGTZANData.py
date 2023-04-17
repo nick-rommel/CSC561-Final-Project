0b6f1f2da3ccfbe4503ff4b818cad93e37ad6ea8
Bolttz <39206340+Bolttz@users.noreply.github.com>
2023-04-17 10:55:04 -0400

    Update LoadGTZANData.py
    all 999 images must be saved in a single folder

--- LoadGTZANData.py
+++ LoadGTZANData.py
@@ -4,8 +4,12 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import Dataset
 from torch.utils.data import random_split
 from torch.utils.data import DataLoader
+
+## img_dir is the folder that holds all the images
 img_dir = 'C:/Users/alexa/Desktop/ELE_Masters/Spring_2023/CSC_561_Neural_Networks_and_Deep_Learning/MusicGenreClassification/ImageFolder/'
 img_labels = os.listdir(img_dir)
+
+## this makes the img_labels, a list of image names, and classifications
 img_labels = [[label,label[:label.index('0')]] for label in img_labels]
 img_labels = [[img[0],0] if img[1] == 'blues'       else img for img in img_labels]
 img_labels = [[img[0],1] if img[1] == 'classical'   else img for img in img_labels]
@@ -22,26 +26,24 @@ img_labels = [[img[0],9] if img[1] == 'rock'        else img for img in img_labe
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self,img_labels,img_dir,transform = None,target_transform = None):
+    def __init__(self,img_labels,img_dir):
         self.img_labels = img_labels
         self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
     def __len__(self):
         return len(self.img_labels)
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels[idx][0])
         image = read_image(img_path)
         label = self.img_labels[idx][1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label,transforms = ToTensor)
         return image, label
 
 CID = CustomImageDataset(img_labels,img_dir)
 
+## We split the 999 images into 800 training, 100 validation and 99 testing images
 train_set,val_set,test_set = random_split(CID,[800,100,99])
+
+## We push the data through the dataloader here, I'm still trying to split them into images and labels
 train_loader = DataLoader(dataset = train_set,batch_size = 64,shuffle = True)
 val_loader = DataLoader(dataset = val_set,batch_size = 64,shuffle = True)
 test_loader = DataLoader(dataset = test_set,batch_size = 64,shuffle = True)
+
