de9c591ceb62f4ee755aba4ac0b9c43d5d4ace76
Bolttz <39206340+Bolttz@users.noreply.github.com>
2023-04-28 20:06:16 -0400

    Update TrainEvalLoop.py
    The file open, write, close step only occurs once per model, so the inefficiency of the function is minimal. A single train session takes ~8 seconds for larger models, so the time to open,write,close is a drop in the bucket

--- TrainEvalLoop.py
+++ TrainEvalLoop.py
@@ -1,16 +1,32 @@
 import torch.optim as TO
 import torch.nn as nn
+import torch
+import time
 
 def TrainNetwork(Network,LR,train_loader,val_loader,test_loader):
+    start = time.time()
     criterion = nn.CrossEntropyLoss(reduction='mean')
-    optimizer = TO.SGD(params = Network.parameters(),lr = LR)
+    optimizer = TO.AdamW(params = Network.parameters(),lr = LR)
     losses = []
     vlosses = []
-    for epoch in range(5):
+    accuracy = []
+    vaccuracy = []
+    for epoch in range(1):
         print(f'Epoch# {epoch+1}')
         Network.train()
-        average_training_loss = Network.trainloop(train_loader,Network,criterion,optimizer)
+        average_training_loss,average_training_accuracy = Network.trainloop(train_loader,Network,criterion,optimizer)
         losses.extend(average_training_loss)
+        accuracy.extend(average_training_accuracy)
         Network.eval()
-        average_validation_loss = Network.validationloop(val_loader,Network,criterion)
+        with torch.no_grad():
+            average_validation_loss,average_validation_accuracy = Network.validationloop(val_loader,Network,criterion)
         vlosses.extend(average_validation_loss)
+        vaccuracy.extend(average_validation_accuracy)
+    name = 'model_criteria'
+    end = time.time()
+    dur = end-start
+    print(name,f'Duration:{dur:0.2f},Acuracy:{accuracy[-1]:.0f},vAcuracy:{vaccuracy[-1]:.0f}',flush = True)
+    filename = 'TrainTest.txt'
+    file = open(filename,'a',encoding='utf-8')
+    file.write(f'{name}\t{losses}\t{vlosses}\t{accuracy}\t{vaccuracy}\t{dur}\n')
+    file.close()
