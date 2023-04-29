48e1ffbdef639719f1a3e8ed49a83e8437fa9d77
Bolttz <39206340+Bolttz@users.noreply.github.com>
2023-04-28 20:38:46 -0400

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,8 +1,16 @@
 import CustomDataloader as CDL
 from MyNet import MyNet as MN
 import TrainEvalLoop as TEL
+import time
 
-path = 'C:/Users/alexa/Desktop/ELE_Masters/Spring_2023/CSC_561_Neural_Networks_and_Deep_Learning/MusicGenreClassification/GTZAN/images_original/'
-train_loader,val_loader,test_loader = CDL.CustomLoader(path,100,800,100,99)
-Network = MN(2,256)
-TEL.TrainNetwork(Network,0.05,train_loader,val_loader,test_loader)
+def train():
+    path = 'C:/Users/alexa/Desktop/ELE_Masters/Spring_2023/CSC_561_Neural_Networks_and_Deep_Learning/MusicGenreClassification/GTZAN/images_original/'
+    train_loader,val_loader,test_loader = CDL.CustomLoader(path,64,800,100,99)
+    Network = MN(2,4*256)
+    TEL.TrainNetwork(Network,0.05,train_loader,val_loader,test_loader)
+
+if __name__ =='__main__':
+    start = time.time()
+    train()
+    end = time.time()
+    print(end-start)
