4dedb3609ba27d5b5f7d13c09630416a27403678
Alex <39206340+Bolttz@users.noreply.github.com>
2023-05-06 14:15:36 -0400

    Add files via upload

--- CustomDataloader.py
+++ CustomDataloader.py
@@ -2,6 +2,7 @@
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets,transforms
 
+
 # function for setting up the different dataloaders for the train, val, and test data splits
 # Inputs:
 #   batch_size: the batch size
@@ -15,7 +16,9 @@ def CustomLoader(path,batch_size,train_size,val_size,test_size):
     # we need to resize the data to 224x224 to fit into the ViT
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Resize([224,224],antialias=True)
+        transforms.CenterCrop([218,348]),# we get 218x348 image centered on the original image. This get rid of most of the 'white' border, but still leaves 10pixels width on the left side of the image
+        transforms.Resize([224,224],antialias=True),
+        transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]) #this normilization was taken from PyTorch Vit_B_16 transforms information
         ])
     
     # executing the data transform
@@ -30,3 +33,4 @@ def CustomLoader(path,batch_size,train_size,val_size,test_size):
 
     # returning the dataloaders
     return train_loader,val_loader,test_loader
+
