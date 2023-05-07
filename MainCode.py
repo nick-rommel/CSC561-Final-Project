75d3177647fa89095875e6befb94f76de87a27cf
Alex <alexander.cerullo96@gmail.com>
2023-05-06 21:51:18 -0400

    forgot to push

--- MainCode.py
+++ MainCode.py
@@ -13,14 +13,14 @@ def train():
     
     # creating the dataloaders by invoking their function.
     # 64 images per patch, 800 train images, 100 validation, 99 test
-    train_loader,val_loader,test_loader = CDL.CustomLoader(path,64,800,100,99)
+    train_loader,val_loader,test_loader = CDL.CustomLoader(path,100,800,100,99)
     # instantiating the network.
     # TODO explain the hard-coded values
     Network = VN(1,4*256)
 
     # training the network using the different dataloaders.
     # learning rate = 0.05
-    TEL.TrainNetwork(Network,0.05,train_loader,val_loader,test_loader)
+    TEL.TrainNetwork(Network,0.001,train_loader,val_loader,test_loader)
 
 # this file is the "main" function of the project, and is the entry point for execution.
 if __name__ =='__main__':
